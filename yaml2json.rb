c2fe16b0f37808477b786409e7e31bde96985f63
Rafael Bodill <justrafi@gmail.com>
2018-11-06 18:58:46 +0200

    Track yaml2json

--- yaml2json.rb
+++ yaml2json.rb
@@ -3,8 +3,8 @@ class Yaml2json < Formula
   homepage "https://github.com/bronze1man/yaml2json"
   head "https://github.com/bronze1man/yaml2json.git"
   url "https://github.com/bronze1man/yaml2json.git",
-      :tag => "v1.2",
-      :revision => "5c26c4289d7a1ea54372e39dc3dee094e85ec445"
+      :tag => "v1.3",
+      :revision => "a361dee9f0c07a192c53114f47c23024f2b697bb"
 
   depends_on "go" => :build
 
