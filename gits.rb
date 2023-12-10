0a42a44a70a3b0068542ce826a911c3cd56e130b
Rafael Bodill <justrafi@gmail.com>
2023-12-10 14:05:09 +0200

    chore: update gits

--- gits.rb
+++ gits.rb
@@ -1,21 +1,26 @@
 class Gits < Formula
   desc "a fast cli git manager for multiple repositories"
   homepage "https://github.com/rafi/gits"
-  url "https://github.com/rafi/gits/archive/v0.3.5.tar.gz"
-  sha256 "a34302bd67b6bd0ae61613f2d0e1a877e8078dbdf4fbc630ede0f8b7f6546ce4"
-  head "https://github.com/rafi/gits.git", branch: "master"
+  url "https://github.com/rafi/gits.git",
+    tag: "v0.3.5",
+    revision: "a34302bd67b6bd0ae61613f2d0e1a877e8078dbdf4fbc630ede0f8b7f6546ce4"
+  head "https://github.com/rafi/gits.git", branch: "next"
 
-  depends_on "go@1.18" => :build
+  depends_on "go" => :build
+
+  uses_from_macos "ncurses"
 
   def install
-    system "go", "build",
-      "-ldflags",
-      "-s -w -X github.com/rafi/gits/cmd.version=#{version}",
-      *std_go_args
+    # Don't dirty the git tree
+    rm_rf ".brew_home"
 
-    # Install bash completion
-    output = Utils.popen_read("#{bin}/gits completion 2>/dev/null")
-    (bash_completion/"gits-completion.bash").write output
+    arch = Hardware::CPU.intel? ? "amd64" : Hardware::CPU.arch.to_s
+    os = OS.kernel_name.downcase
+
+    system "make", "bin/release/gits-#{os}-#{arch}"
+    bin.install "bin/release/gits-#{os}-#{arch}" => "gits"
+
+    generate_completions_from_executable(bin/"gits", "completion")
   end
 
   test do
