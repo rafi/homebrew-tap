403574bbcf6cedcf1314eb29ee1f91a4f748e0e7
Rafael Bodill <justrafi@gmail.com>
2023-12-10 16:21:30 +0200

    chore: upgrade gits

--- gits.rb
+++ gits.rb
@@ -2,8 +2,8 @@ class Gits < Formula
   desc "a fast cli git manager for multiple repositories"
   homepage "https://github.com/rafi/gits"
   url "https://github.com/rafi/gits.git",
-    tag: "v0.3.5",
-    revision: "a34302bd67b6bd0ae61613f2d0e1a877e8078dbdf4fbc630ede0f8b7f6546ce4"
+    tag: "v0.5.0",
+    revision: "e9c65e65ef3ccbe6784db17bee7ab0322b913e56"
   head "https://github.com/rafi/gits.git", branch: "next"
 
   depends_on "go" => :build
