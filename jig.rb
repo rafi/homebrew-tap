0a43d1b2b1dc01bacc6a9d9ecf62656f528c5db9
Rafael Bodill <justrafi@gmail.com>
2024-05-18 01:16:38 +0300

    feat: add jig formula

---
+++ jig.rb
@@ -0,0 +1,31 @@
+class Jig < Formula
+  desc "tmux launcher"
+  homepage "https://github.com/rafi/jig"
+  url "https://github.com/rafi/jig.git",
+    tag: "v0.1.0",
+    revision: "cef4853e0a3786b981df7c7c1c9019a5d00606f4"
+  head "https://github.com/rafi/jig.git", branch: "next"
+
+  depends_on "go"   => :build
+  depends_on "just" => :build
+
+  uses_from_macos "ncurses"
+
+  def install
+    # Don't dirty the git tree
+    rm_rf ".brew_home"
+
+    arch = Hardware::CPU.intel? ? "amd64" : Hardware::CPU.arch.to_s
+    os = OS.kernel_name.downcase
+
+    system "just", "build", os, arch
+    bin.install "bin/release/jig-#{os}-#{arch}" => "jig"
+
+    bash_completion.install "completion/jig.bash"
+    fish_completion.install "completion/jig.fish"
+  end
+
+  test do
+    system "#{bin}/jig", "version"
+  end
+end
