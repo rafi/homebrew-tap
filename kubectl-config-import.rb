0747e58457f649c4b6290fc6926e52787b3461f3
Rafael Bodill <justrafi@gmail.com>
2024-10-04 16:10:04 +0300

    chore: track kubectl-config-import version

--- kubectl-config-import.rb
+++ kubectl-config-import.rb
@@ -1,6 +1,9 @@
 class KubectlConfigImport < Formula
   desc "Merge kubeconfigs stored as Kubernetes secrets or files"
   homepage "https://github.com/rafi/kubectl-config-import"
+  url "https://github.com/rafi/kubectl-config-import.git",
+    tag: "v0.5.0",
+    revision: "ee5ff603aa1be040f4c176780e27c2b8f8561bb6"
   head "https://github.com/rafi/kubectl-config-import.git", branch: "master"
 
   depends_on "yq"
